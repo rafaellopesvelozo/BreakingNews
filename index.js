6b351ccffe3cfa4e72df91ffcfe4ee7ddea6246b
rafaellopesvelozo <rafaellopesvelozo@gmail.com.br>
2022-09-26 20:07:18 -0300

    request

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
-const userRoute = require("./src/routes/user.route");
 const app = express();
+
+const userRoute = require("./src/routes/user.route");
 
 const PORT = 3000;
 
-app.use('/soma', userRoute)
-
+//enviar e receber arquivos json
+app.use(express.json())
+app.use("/user", userRoute);
 
 app.listen(PORT, () =>
   console.log(`servidor rodando na porta http://localhost:${PORT}`)
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,23 @@
-const soma = (req, res) => {
-    res.send('oi')
+const create = (req, res) => {
+    const {name, username, email, password, avatar, background} = req.body
+
+    if( !name || !username || !email || !password || !avatar ||!background ){
+        res.status(400).send({message: "submuit all fields for registration"})
+    }
+
+    res.status(201).send({
+        message: "user created successfully",
+        user:{
+            name,
+            username,
+            email,
+            avatar,
+            background,
+        },
+      
+    })
 };
 
 module.exports = {
-  soma,
+    create,
 };
