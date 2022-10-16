01a4b9d1b64794bde15f84d5a3ce17eb07b0f754
rafaellopesvelozo <rafaellopesvelozo@gmail.com.br>
2022-10-16 20:07:53 -0300

    Fetching the first data of a list

--- src/routes/news.route.js
+++ src/routes/news.route.js
@@ -2,12 +2,10 @@ import { Router } from "express";
 const router = Router();
 
 import { authMiddleware } from "../middlewares/auth.middlewares.js";
-import {create, findAll} from "../controllers/news.controller.js"
+import { create, findAll, topNews } from "../controllers/news.controller.js";
 
-
-
-router.post("/", authMiddleware, create)
-router.get("/", findAll)
-
+router.post("/", authMiddleware, create);
+router.get("/", findAll);
+router.get("/top", topNews);
 
 export default router;
--- src/services/news.service.js
+++ src/services/news.service.js
@@ -3,8 +3,10 @@ import News from "../models/News.js";
 const createService = (body) => News.create(body);
 const findAllService = (offset, limit) => News.find().sort({_id: -1}).skip(offset).limit(limit).populate("user");
 const countNews = () => News.countDocuments()
+const topNewsService = () => News.findOne().sort({_id: -1}).populate("user")
 export {
   createService,
   findAllService,
   countNews,
+  topNewsService,
 };
